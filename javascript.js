6448e76a2e34f3c345579db49633fa36750cc30c
tamekamclean <tamekasmclean@gmail.com>
2022-10-02 18:45:05 -0400

    add page structure for quiz

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
-function click(){
+// const gamePlayer = 8
 
-}
-var clickMe
-clickMe.addEventListener("click", click())
+// if (gamePlayer === 10){
+//     console.log("You won!");
+// }else if(gamePlayer > 5) {
+//     console.log("Sorry Try Again");
+// }else{(gamePlayer < 5)
+//     console.log("Almost there");
+// }
+
+// When the player gets the number correct a part of the term 
+// You Just Won appears
+
+// i need a local storage for the game window that stores the word recieved from the right answer
+// i need the page to light up when the players window. 
+
+// const x = 2
+// const y = 4
+// const z = 6
+// const t = 8
+
+// alert(t)
+
+console.log(document.getElementsByClassName("modal-dialog"));
